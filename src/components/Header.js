a89b9e930a11f6cbed8eabe2e638d7c39a7ce868
Ligia de Almeida Silva <ligiaalmeidasilva@gmail.com>
2019-07-29 01:01:54 -0300

    Criado Header.js
    Criado o comp Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/App.css';
+
+const Header = () => (
+  <header>
+    <h1 className="title">Repositories List</h1>
+  </header>
+)
+
+export default Header;
